5bf4f0919fada25729d34f21771c1d5a5b0bed67
Roberta Bustos <rbbustos3@gmail.com>
2025-01-04 07:48:15 -0600

    primitives

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -8,7 +8,8 @@ fn main() {
 
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    // let …'='
+    let is_evening = !is_morning;
     if is_evening {
         println!("Good evening!");
     }
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -16,6 +16,15 @@ fn main() {
     // You can optionally experiment here.
 }
 
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    // You can optionally experiment here.
+    if num_apples > 40 {
+        num_apples
+    } else {
+        num_apples * 2
+    }
+}
+
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
